ed4f0d2bb65fc12e2c64be65c4a06e32ad20db83
Aryan <aryansharma220318@gmail.com>
2023-11-27 21:11:50 +0530

    added the footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='shadow-md w-full h-auto md:h-24 px-2 md:px-4 bg-black opacity-100 flex items-center justify-center'>
+            <p className='text-slate-300 font-bold '>
+              Created by Aryan Sharma
+            </p>
+    </footer>
+  )
+}
+
+export default Footer
